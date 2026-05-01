b2f03e64a5c631ebcebff5b4e2690b69dd62927f
PrinnoTech198 <innocent.pachalio@gmail.com>
2026-05-01 20:57:58 +0300

    update requiremen73

--- mpanzi/settings.py
+++ mpanzi/settings.py
@@ -109,11 +109,10 @@ TEMPLATES = [
 ]
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.getenv("CLOUD_NAME"),
-    'API_KEY': os.getenv("API_KEY"),
-    'API_SECRET': os.getenv("API_SECRET"),
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('API_KEY'),
+    'API_SECRET': os.environ.get('API_SECRET'),
 }
-
 WSGI_APPLICATION = "mpanzi.wsgi.application"
 DB_LIVE = os.getenv("DB_LIVE")
 
