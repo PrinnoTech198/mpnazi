485eb10407d975e8e1a90ada428cf5c8d18ec74e
PrinnoTech198 <innocent.pachalio@gmail.com>
2026-05-23 11:12:31 +0300

    change port

--- mpanzi/settings.py
+++ mpanzi/settings.py
@@ -299,7 +299,7 @@ EMAIL_BACKEND = os.environ.get(
     "EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend"
 )
 EMAIL_HOST = os.environ.get("EMAIL_HOST","smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 465))
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS","true").lower() in ("1", "true", "yes")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER","innocentphilbert39@gmail.com")
 # Gmail app passwords are often pasted with spaces — strip them.
